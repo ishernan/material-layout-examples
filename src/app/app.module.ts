b787f5908432fdc2527118a6f0c178f405337ac5
Isaac Hernandez <isaac.hernandez@outlook.com>
2022-05-05 22:33:51 +0200

    not working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FlexLayoutModule } from '@angular/flex-layout';
+import { CoreModule, FlexLayoutModule } from '@angular/flex-layout';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatButtonModule} from '@angular/material/button';
+//import { LoginComponent } from './login/login.component';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+     AppComponent,
+    // LoginComponent
   ],
+  
   imports: [
+    CoreModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
     MatFormFieldModule,
     MatMenuModule,
-    MatButtonModule
+    MatButtonModule,
+    FormGroup,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,30 @@
+// import { Component, OnInit } from '@angular/core';
+// import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+// @Component({
+//   selector: 'app-login',
+//   templateUrl: './login.component.html',
+//   styleUrls: ['./login.component.scss']
+// })
+// export class LoginComponent implements OnInit {
+
+//   public loginForm!: FormGroup;
+//   public username!: string; 
+//   public password!: string; 
+
+
+//   constructor(private formBuilder: FormBuilder) {   
+//    }
+
+//   ngOnInit(): void {
+//     this.loginForm = this.formBuilder.group({
+//       username: ['', Validators.required],
+//       password: ['', Validators.required],
+//     });
+//   }
+
+//   onSignin() {
+//     return console.log("sended")
+//   }
+
+// }
